2cb26585a5f43db64d7ac65cdb05d9ffaed64adb
KRBhavaniSankar <krbhavanisankar@gmail.com>
2019-04-04 17:41:27 +0900

    Exactracting data from private S3 bucket.

--- ApacheSpark/SparkSQL/DataExtractionFromPrivateS3Bucket.scala
+++ ApacheSpark/SparkSQL/DataExtractionFromPrivateS3Bucket.scala
@@ -4,52 +4,56 @@ import org.joda.time.format.DateTimeFormat
 
 object AIEventLogData extends App {
 
-  val accessKey = args(0)
-  val accessSecretKey = args(1)
-  val s3Path = args(2)
-  val collectionBucket = args(3)
-  val backupBucket = args(4)
-  val startDate = args(5)
-  val endDate= args(6)
-  val outputPath = args(7)
+val accessKey = args(0)
+ val accessSecretKey = args(1)
+ val s3Path = args(2)
+ val collectionBucket = args(3)
+ val backupBucket = args(4)
+ val startDate = args(5)
+ val endDate= args(6)
+ val outputPath = args(7)
 
-  val logType = "ai_event_log"
+ val logType = "ai_event_log"
 
-  val originalFormat = DateTimeFormat.forPattern("yyyyMMdd")
-  val newStartDate = originalFormat.parseDateTime(startDate)
-  val newEndDate= originalFormat.parseDateTime(endDate)
+ val originalFormat = DateTimeFormat.forPattern("yyyyMMdd")
+ val newStartDate = originalFormat.parseDateTime(startDate)
+ val newEndDate= originalFormat.parseDateTime(endDate)
 
-  val durationDays = Days.daysBetween(newStartDate,newEndDate).getDays()
+ val durationDays = Days.daysBetween(newStartDate,newEndDate).getDays()
 
-  val tempS3Path = (0 to durationDays).map(date => {
-    val newDate = newStartDate.plusDays(date)
-    val newTargetDate = newDate.toString("yyyyMMdd")
-    val targetMonth = newDate.toString("yyyyMM")
-    s"${targetMonth}/${newTargetDate}/t_${logType}_${newTargetDate}_*"
-  })
+ val tempS3Path = (0 to durationDays).map(date => {
+   val newDate = newStartDate.plusDays(date)
+   val newTargetDate = newDate.toString("yyyyMMdd")
+   val targetMonth = newDate.toString("yyyyMM")
+   s"${targetMonth}/${newTargetDate}/t_${logType}_${newTargetDate}_*"
+ })
 
-  val finalS3Path = tempS3Path.mkString(",")
-  val collectionS3Path = s"${s3Path}/${collectionBucket}/{${finalS3Path}}"
-  val spark = SparkSession.builder()
-    .appName(s"Copy Data : ${args.mkString(", ")}")
-    .getOrCreate()
+ val finalS3Path = tempS3Path.mkString(",")
+ val docomoCollectionS3Path = s"${s3Path}/${collectionBucket}/{${finalS3Path}}"
 
-  spark.sparkContext.setLogLevel("ERROR")
-  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", accessKey)
-  spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", accessSecretKey)
+ println(docomoCollectionS3Path)
 
-  import spark.implicits._
+ val spark = SparkSession.builder()
+   .appName(s"Copy Data : ${args.mkString(", ")}")
+   .config("spark.hadoop.io.compression.codecs", "com.asurion.codec.GzipLoadCodec")
+   .getOrCreate()
 
-  val aiEventDf = spark.read
-    .option("header","false")
-    .option("inferSchema","true")
-    .option("timestampFormat","yyyy/MM/dd HH:mm:ss")
-    .csv(collectionS3Path)
-    .distinct()
+ spark.sparkContext.setLogLevel("ERROR")
+ spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", accessKey)
+ spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", accessSecretKey)
 
-  aiEventDf.write.parquet(outputPath)
-  spark.stop()
-  println("------------END--------------------")
+ import spark.implicits._
 
+ val aiEventDf = spark.read
+   .option("header","false")
+   .option("inferSchema","true")
+   .option("timestampFormat","yyyy/MM/dd HH:mm:ss")
+   .csv(docomoCollectionS3Path)
+   .distinct()
+
+
+ aiEventDf.coalesce(180).write.mode(SaveMode.Append).parquet(outputPath)
+ spark.stop()
+ println("------------END--------------------")
 
 }
