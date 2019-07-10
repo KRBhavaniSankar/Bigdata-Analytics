13db74592706dd795a0302f48daf29add56dcc4f
KRBhavaniSankar <krbhavanisankar@gmail.com>
2019-07-10 17:32:41 +0900

    Sample queries to perform nested_arrays, inner join operations using GCP warehouse bigquery using Classic WEBUI

--- GoogleCloudPlatform/BigQuery/test3_extracting_nested_array_data.sql
+++ GoogleCloudPlatform/BigQuery/test3_extracting_nested_array_data.sql
@@ -1,5 +1,5 @@
 SELECT event_date,event_timestamp,event_name,device.mobile_os_hardware_model,device.advertising_id,
 ARRAY(SELECT event_params.key FROM UNNEST(event_params) AS event_params) AS event_param_key,
 (SELECT ARRAY_AGG(event_params.value.string_value IGNORE NULLS) FROM UNNEST(event_params) AS event_params) AS event_param_value
-FROM `docomomail-dcef6.analytics_165653046.events_20181218`
+FROM `project-name.analytics_165653046.events_20181218`
 LIMIT 100
